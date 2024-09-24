e26055aa6b1754c08b41512bac68da3bb12fe845
PRO X <bredalisgautreaux@gmail>
2024-09-24 07:19:46 +0200

    feat: ✨ Nuevo programa

--- Scripts/Conjuntos.py
+++ Scripts/Conjuntos.py
@@ -14,7 +14,3 @@ print("Intersección de A y B:", interseccion)  # {4, 5}
 # Diferencia: elementos de A que no están en B
 diferencia = conjunto_A.difference(conjunto_B)
 print("Diferencia de A y B:", diferencia)  # {1, 2, 3}
-
-# Diferencia simétrica: elementos que están en A o B, pero no en ambos
-diferencia_simetrica = conjunto_A.symmetric_difference(conjunto_B)
-print("Diferencia simétrica de A y B:", diferencia_simetrica)  # {1, 2, 3, 6, 7, 8}
