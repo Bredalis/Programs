7a2e677eb997c20df0fa95bcf2afda3860093801
Bredalis <bredalisgautreaux@gmail.com>
2025-08-27 09:16:17 +0200

    delete y refactor: ✨ Se borró la carpeta BBDD. Se modificó el archivo Codificación_Ascii.

--- Scripts/Codificacion_Ascii.py
+++ Scripts/Codificacion_Ascii.py
@@ -1,16 +1,17 @@
 
-import tensorflow as tf
-
 print("Codificar y decodificar ASCII")
+
 def codificar(texto: str):
   try:
-    texto = tf.io.decode_raw(tf.constant(texto), tf.uint8).numpy()
-    return " ".join(str(letra) for letra in texto)
+
+    # Convierte cada caracter en su valor ASCII
+    return " ".join(str(ord(letra)) for letra in texto)
   except Exception as e:
     return f"Error inesperado: {e}"
 
 def decodificar(texto: str):
   try:
+    # Convierte cada número en caracter ASCII
     return "".join(chr(int(letra)) for letra in texto.split())
   except Exception as e:
     return f"Error inesperado: {e}"
